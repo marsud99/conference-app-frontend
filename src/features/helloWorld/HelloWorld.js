3f58174803e58e3ad829b44ec3bbfe731d7fd3e2
professionalstreetnig <marian.suditu99@gmail.com>
2021-09-28 16:00:42 +0300

    modified with graphql

--- src/features/helloWorld/HelloWorld.js
+++ src/features/helloWorld/HelloWorld.js
@@ -1,8 +1,17 @@
 
+import React from 'react';
+import { LoadingFakeText } from '@bit/totalsoft_oss.react-mui.kit.core';
+import { useQuery } from '@apollo/client';
+import { MY_FIRST_QUERY } from './queries/MyFirstQuery';
 
+function HelloWorld() {
+    const { loading, data } = useQuery(MY_FIRST_QUERY);
 
-function HelloWorld(){
-    return "Hello World🐱‍👤";
+    if (loading) {
+        return <LoadingFakeText lines={10} />
+    }
+
+    return data?.myFirstEndpoint
 }
 
 export default HelloWorld;
